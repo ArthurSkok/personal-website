52d2b0cd0c3afed542cd7d30da7c7de338088433
ArthurSkok <arthur@skok.us>
2025-02-23 19:58:58 -0500

    getting rid of server code in react renders

--- src/Pages/Popup.js
+++ src/Pages/Popup.js
@@ -1,27 +1,6 @@
 import React from "react";
 import "./Popup.css";
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
-
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-    // Broadcast the message to all connected clients
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-});
-
 const Popup = ({ show, onClose, input }) => {
   if (!show) {
     return null;
